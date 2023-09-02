f5dcf2b3c50e8d19ecfa90c3cd877f28133c62c8
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-09-02 13:12:42 +0600

    added more functionalities

--- js/script.js
+++ js/script.js
@@ -20,40 +20,43 @@ const secondsToMinutes = (seconds) => {
 }
 
 // get category buttons
-const getCategoryButtons = async () => {
+const loadCategoryButtons = async () => {
     const categoryContainer = getElement("category-btn-container");
+    categoryContainer.innerHTML = "";
 
-    // loading categories
+    // load categories
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await res.json();
     const categories = data.data;
     
-    // setting category buttons in UI
+    // display category buttons
     for(let category of categories) {
         const categoryButtonDiv = document.createElement("div");
         categoryButtonDiv.classList.add("inline-block");
         categoryButtonDiv.innerHTML = `
-            <button onclick="showCategoryContent('${category.category_id}')" class="btn normal-case font-normal px-7 mx-3 text-[17px] category-btn">${category.category}</button>
+            <button onclick="loadCardContents('${category.category_id}')" class="btn normal-case font-normal px-[20px] mx-[10px] text-[17px] category-btn">${category.category}</button>
         `;
         categoryContainer.appendChild(categoryButtonDiv);
     }
 }
 
 // handle category button click
-const showCategoryContent = async (id) => {    
+const loadCardContents = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
     const cards = data.data;
 
+    displayCards(cards);
+}
+
+// show cards
+const displayCards = (cards) => {
     const videosContainer = getElement("videos-container");
     videosContainer.innerHTML = "";
 
     if (cards.length > 0) {
         videosContainer.classList.add("grid");
-
-        // console.log(cards[0]);
-
-        for(const card of cards) {
+        for (const card of cards) {
             const hours = secondsToHours(parseInt(card.others.posted_date));
             const minutes = secondsToMinutes(parseInt(card.others.posted_date));
 
@@ -65,7 +68,7 @@ const showCategoryContent = async (id) => {
                 </div>
                 <div class="flex">
                     <div class="mr-4">
-                        <img src="${card.authors[0].profile_picture}" alt="author" class="inline-block w-14 h-14 rounded-full">
+                        <img src="${card.authors[0].profile_picture}" alt="author" class="inline-block w-[50px] h-[50px] rounded-full">
                     </div>
                     <div>
                         <h4 class="text-xl font-semibold">${card.title}</h4>
@@ -79,10 +82,14 @@ const showCategoryContent = async (id) => {
     } else {
         videosContainer.classList.remove("grid");
         videosContainer.innerHTML = `
-            <h1 class="text-3xl font-semibold text-center">Oops!! Sorry, There is no content here</h1>
+        <div class="flex items-center justify-center error-container">
+            <div class="text-center">
+                <div class="flex justify-center"><img src="./images/Icon.png" alt="icon" class="inline-block mb-10"></div>
+                <h1 class="text-3xl font-semibold">Oops! Sorry!!! Content unavailable!</h1>
+            </div>
+        </div>
         `;
     }
 }
 
-getCategoryButtons();
-showCategoryContent(1000);
+loadCategoryButtons();
